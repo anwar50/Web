c9cc918c7f376526297077aa99e5197047cb3eba
anwar50 <37552972+anwar50@users.noreply.github.com>
2018-03-22 11:31:08 +0000

    Update Todo.js

--- Todo.js
+++ Todo.js
@@ -1,12 +1,22 @@
 function todoList() {
-	// store user value
-	var item    = document.getElementById('todoInput').value
-	// create a text node from the user input
-	var text    = document.createTextNode(item)
+	// store the subject user inputs
+	var subject    = document.getElementById('Input').value
+	
+	// store the date the user inputs
+	var date    = document.getElementById('Input2').value
+	
+	// create a text node from the user for the subject
+	var text    = document.createTextNode(subject)
+	
+	// create a text node from the user for the subject
+	var text2    = document.createTextNode(date)
+	
 	// create a li tag
-	var newItem = document.createElement('li')
+	var newText = document.createElement('li')
+	
 	// add the user input to the li tag
-	newItem.appendChild(text)
+	newText.appendChild(text)
 	// append the li to the html todoList id tag
-	document.getElementById('todoList').appendChild(newItem)
+	document.getElementById('List').appendChild(newText)
+	document.getElementById('DateList').appendChild(newText)
 }
